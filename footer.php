70cd9e4c6ddc7c191d8f00443b5c014dd3679429
Credos <ozcelikgamze708@gmail.com>
2025-12-19 18:22:37 +0300

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-<footer class="footer">
-    <p>&copy; <?php echo date("Y"); ?> EVURT CRAFT Projesi. Tum Haklari Saklidir. | Powered by CREDOS Discord:credos_.</p>
-</footer>
-</body>
-</html>
